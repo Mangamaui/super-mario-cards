411dba8f14438404537f57f4d2871ea279a30731
Claudia Reynders <exotic.maui@gmail.com>
2019-01-20 14:58:50 +0100

    Added views to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {view: this.switchView("start")};
+
+  }
+
+  switchView (view) {
+      switch(view) {
+        case "start":
+          return (
+            <div className="view">
+              <h2>Welcome to Silly Cardgame!</h2>
+              <button onClick={this.handleClick}>Start Game</button>
+            </div>
+          )
+
+        default:
+          return (<div>404</div>);
+      }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const view = this.switchView("game");
+
+    this.setState({
+      view: view
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Silly Cardgame</h1>
         </header>
+        <main className="main">
+          {this.state.view}
+        </main>
       </div>
     );
   }
