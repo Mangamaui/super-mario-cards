6d345227e7530abecc4e444726b6111759e3dcba
Claudia Reynders <exotic.maui@gmail.com>
2019-01-20 14:56:21 +0100

    Created viewSwitcher

---
+++ src/app/views/index.js
@@ -0,0 +1,19 @@
+import 'React' from 'react';
+
+class view extends React.Component {
+  selectView(view") {
+    switch (view) {
+      case "start":
+
+        break;
+      default:
+
+    }
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
