0a47d02968cfb786582dd1f997df525f8f83a979
Claudia Reynders <exotic.maui@gmail.com>
2019-01-20 15:01:01 +0100

    Added more card reducers
    - add card
    - update card
    - update selected card
    - clear selected card

--- src/app/rootreducer.js
+++ src/app/rootreducer.js
@@ -17,6 +17,26 @@ const rootReducer = (state = initialState, action) => {
       console.log("create game");
       return {...initialState};
 
+    case 'ADD_CARD':
+      console.log("add card");
+      cardList = [...state.cardList, action.payload];
+      return {...state, cardList: cardList};
+
+    case 'UPDATE_CARD':
+      console.log("update card");
+      cardList = [...state.cardList];
+      const index = cardList.findIndex(card => card.id === action.payload);
+      cardList[index].state = 1 ;
+
+      return {...state};
+
+    case 'UPDATE_SELECTED_CARD':
+      console.log("update previous selected cards");
+      return {...state, previousSelectedCard: action.payload};
+
+    case 'CLEAR_SELECTED_CARD':
+      console.log("cleared previous selected card");
+      return {... state, previousSelectedCard: null};
 
     default:
        return state;
