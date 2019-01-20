049386d90f230d533e6618560c37fcef6285df8e
Claudia Reynders <exotic.maui@gmail.com>
2019-01-20 15:01:43 +0100

    Added more card-related actions
    - add card
    - update card
    - update selected card
    - clear selected card

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,3 +3,30 @@ export function createGame() {
     type: 'CREATE_GAME'
   }
 }
+
+export function addCard(card) {
+  return {
+    type: 'ADD_CARD',
+    payload: card
+  }
+}
+
+export function updateCard(id) {
+  return {
+    type: 'UPDATE_CARD',
+    payload: id
+  }
+}
+
+export function updateSelectedCard(id) {
+  return {
+    type: 'UPDATE_SELECTED_CARD',
+    payload: id
+  }
+}
+
+export function clearSelectedCard() {
+  return {
+    type: 'CLEAR_SELECTED_CARD'
+  }
+}
