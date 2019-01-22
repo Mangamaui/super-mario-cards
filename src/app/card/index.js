1a9ef1b829e2a7eb1d76f87559eb4619562c7e9d
Claudia Reynders <exotic.maui@gmail.com>
2019-01-22 19:11:16 +0100

    Added conditional rendering to card

--- src/app/card/index.js
+++ src/app/card/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { CARD_STATES } from './../constants';
 
 const Card = (props) => {
-
+  console.log(props.state);
   return (
     <div className="card" id={props.id} onClick={props.cardHandler}>
-      {props.sign}
+
+        {props.state === CARD_STATES.VISIBLE &&
+        props.sign}
     </div>
   )
 }
