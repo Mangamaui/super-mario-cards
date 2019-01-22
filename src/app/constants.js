be357d154070eb14bc5afbe0346b2f3b6dc5e107
Claudia Reynders <exotic.maui@gmail.com>
2019-01-22 19:10:11 +0100

    Created constants
    Created a file to group all constants data in the app

---
+++ src/app/constants.js
@@ -0,0 +1,14 @@
+export const CARD_STATES = {
+  HIDDEN: "hidden",
+  VISIBLE: "visible",
+  INACTIVE: "inactive"
+};
+
+export const CARD_TYPES = [
+  ["flower",  4],
+  ["mushroom", 4],
+  ["star", 4],
+  ["life", 2],
+  ["coin10", 2],
+  ["coin20", 2]
+];
