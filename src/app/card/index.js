83f367582c8149bbb1a180e5b04cb8019bec0c50
Claudia Reynders <exotic.maui@gmail.com>
2019-01-24 00:34:12 +0100

    Refactored and added conditional card rendering

--- src/app/card/index.js
+++ src/app/card/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { CARD_STATES } from './../constants';
 
 const Card = (props) => {
-  console.log(props.state);
-  return (
-    <div className="card" id={props.id} onClick={props.cardHandler}>
+  let card;
 
+  if(props.state === CARD_STATES.INACTIVE) {
+    card = (
+      <div className="card" id={props.id}>
+        {props.sign}
+      </div>
+    )
+  } else {
+    card = (
+      <div className="card" id={props.id} onClick={props.cardHandler}>
         {props.state === CARD_STATES.VISIBLE &&
         props.sign}
-    </div>
+      </div>
+    )
+  }
+
+  return (
+    <React.Fragment>
+      {card}
+    </React.Fragment>
   )
 }
 
