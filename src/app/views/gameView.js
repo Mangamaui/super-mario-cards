8e2d66d2d3a3c651455a48f0eeaa4d47bbfce443
Claudia Reynders <exotic.maui@gmail.com>
2019-02-03 21:18:35 +0100

    Added gameView

---
+++ src/app/views/gameView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Playfield from '../playfield';
+
+
+const GameView = () => {
+  return (
+    <React.Fragment>
+      <h2>Find 2 identical cards</h2>
+      <Playfield />
+    </React.Fragment>
+  )
+}
+
+export default GameView;
